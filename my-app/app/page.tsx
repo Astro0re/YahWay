c0638a6217a132963f5b8c65ea84566579ceeafe
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-03-22 19:13:49 +0100

    Layout attempt

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -49,6 +49,7 @@ export default function Home() {
 </NavigationMenu>
   </div>
   
+  <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
   <Image
       src="/Yahway Logo.svg"
       alt="logo"
@@ -84,7 +85,7 @@ export default function Home() {
         <h3>Our Mission</h3>
         <p>To fufill the great commision.</p>
     </div> 
-    
+    </div>
 
       
 

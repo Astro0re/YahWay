8f038ad11a256d5be14f18bb84b76b4ae614d8ba
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-03-22 19:08:28 +0100

    Added Content

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -47,7 +47,8 @@ export default function Home() {
     </NavigationMenuItem>
   </NavigationMenuList>
 </NavigationMenu>
-
+  </div>
+  
   <Image
       src="/Yahway Logo.svg"
       alt="logo"
@@ -64,10 +65,16 @@ export default function Home() {
         <p>It was written in the stars</p>
         <p>Note: Starry (GOD X YOU) Design</p>
     </div>
+    <div id="What-we-do">
+    <h2>Our Services</h2>
+        <p>Get in contact with the church of your choice closest to you.</p>
+        <p>For all your chirstian fits and essentials.</p>
+        <p>Get tickets to impactful events that are sure to change your life.</p>  
+    </div>
     <div id="Info">
         <h2>What do we do?</h2>
         <p>We offer christian community building services</p>
-        <p>Allowign for the connection of more people to the love of God.</p>
+        <p>Allowing for the connection of more people to the love of God.</p>
        
         <h3>Our Goal</h3>
         <p>Connect our users to christ loving chruches closest to them(map)</p>
@@ -77,7 +84,8 @@ export default function Home() {
         <h3>Our Mission</h3>
         <p>To fufill the great commision.</p>
     </div> 
-  </div>
+    
+
       
 
       
@@ -96,18 +104,6 @@ export default function Home() {
         <h2>LET US HELP YOU GET CLOSER TO GOD TODAY</h2>
         <p>With our carefully scouted and trusted church partners</p>
         <p> Let us connect you with a church to call home</p>
-
-        <h2>What do we do?</h2>
-        <p>We offer christian community building services</p>
-        <p>Allowign for the connection of more people to the love of God.</p>
-        
-        <h3>Our Goal</h3>
-        <p>Connect our users to christ loving chruches closest to them(map)</p>
-        <p>Provide a platform for the sale of christian essentials(store)</p>
-        <p>Give access to life changing events(events)</p>
-        
-        <h3>Our Mission</h3>
-        <p>To fufill the great commision.</p>
 
       </main>
 

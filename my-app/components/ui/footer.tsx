59a514551dc768274ce18e65c0bcb2297e2323d5
Fesobi Oreofeoluwa <159962465+Astro0re@users.noreply.github.com>
2025-05-20 13:40:03 +0100

    Create footer.tsx

---
+++ my-app/components/ui/footer.tsx
@@ -0,0 +1,18 @@
+export defult function footer() {
+  {
+    <h1>Contact Us</h1>
+    <p>Reach out to us on these platforms</p>
+    <div>
+      <div>
+      <p>Address</p>
+      </div>
+      <div>
+      <p>Socials</p>
+      </div>
+      <div>
+      <p>Number</p>
+      <p>Email</p>
+      </div>
+    </div>
+  }
+}

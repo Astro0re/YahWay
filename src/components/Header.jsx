bab36a71665d7ac078f6c99829539e01d61a92a1
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-05-20 18:58:44 +0100

    framework test

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return( 
+        <header>
+            
+        <h1>Welcome to YahWay</h1>
+        <p>LET'S HELP YOU GET CLOSER TO GOD TODAY</p>
+        <p>With our carefully scouted and trusted church partners</p>
+        <p>Let's connect you with a church to call home</p>
+        </header>
+        
+    )
+       
+    
+}

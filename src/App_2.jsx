d55ce3e9dcb610ca10eca68a2a428dfea9c0637f
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-08-10 18:04:17 +0100

    Test

---
+++ src/App_2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import YahwayLogo from './assets/Yahway Logo.svg'
+
+export default function App_2() {
+    return (
+        <>
+        <h1>Welcome to YahWay</h1>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import App_2 from './App_2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <App_2 />
   </StrictMode>,
 )

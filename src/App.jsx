badb16c57e886dcc60ef58e5b02fbb76eca4fb22
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-08-20 13:39:50 +0100

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Store from './components/Store'
 import yahwaylogo from './assets/Yahway Logo.svg'
+import Events from './components/Events'
 
 function App() {
 
 	return (
 		<>
 			<img src={yahwaylogo}></img>
+			<nav>
+				<ul className="flex justify-center space-x-4 bg-gray-800 p-4 text-white">
+					<li><a href="#store">Store</a></li>
+				</ul>
+			</nav>
 			<Header/>
 			<Store />
+			<Events />
 			<Footer />
 		</>
 	)

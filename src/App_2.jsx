70e9abd38ba752c7fa6ecbf659222f21f6e53b43
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-08-21 18:40:30 +0100

    component edit

--- src/App_2.jsx
+++ src/App_2.jsx
@@ -4,7 +4,7 @@ import YahwayLogo from './assets/Yahway Logo.svg'
 export default function App_2() {
     return (
         <>
-        <h1>Welcome to YahWay</h1>
+        <h1></h1>
         </>
     )
 }
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,34 +1,33 @@
-
-export default function event(){
+export default function Events(){
     return(
         <>
-            <section  className="bg-white p-6 m-2 rounded-lg shadow gap-5">
+            <section className="bg-white p-6 m-2 rounded-lg shadow gap-5">
         
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5 m-4 justify-center">
 
             <div className="bg-black text-center text-white p-10 rounded">
-            <img  alt="Community" class="h-fit w-fit rounded p-5"></img>
+            <img  alt="Community" className="h-fit w-fit rounded p-5"></img>
             <div className="flex flex-row gap-20 justify-center p-4 bg-white text-black m-2 rounded">
                 <p>Event 1 </p>
             </div>
             </div>
 
             <div className="bg-black text-center text-white p-10 rounded">
-            <img  alt="Community" class="h-fit w-fit rounded p-5"></img>
+            <img  alt="Community" className="h-fit w-fit rounded p-5"></img>
             <div className="flex flex-row gap-20 justify-center p-4 bg-white text-black m-2 rounded">
                 <p>Event 2 </p>
             </div>
             </div>
 
             <div className="bg-black text-center text-white p-10 rounded">
-            <img  alt="Community" class="h-fit w-fit rounded p-5"></img>
+            <img  alt="Community" className="h-fit w-fit rounded p-5"></img>
             <div className="flex flex-row gap-20 justify-center p-4 bg-white text-black m-2 rounded">
                 <p>Event 3 </p>
             </div>
             </div>
 
             <div className="bg-black text-center text-white p-10 rounded">
-            <img  alt="Community" class="h-fit w-fit rounded p-5"></img>
+            <img  alt="Community" className="h-fit w-fit rounded p-5"></img>
             <div className="flex flex-row gap-20 justify-center p-4 bg-white text-black m-2 rounded">
                 <p>Event 4 </p>
             </div>

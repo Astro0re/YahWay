7af02192ededbc86735e7c7f6c1d9eda72f4c18b
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-08-22 14:15:52 +0100

    New page

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,11 @@ function App() {
 
 	return (
 		<>
+			<h1>Welcome</h1>
 			<img src={yahwaylogo}></img>
 			<nav>
 				<ul className="flex justify-center space-x-4 bg-gray-800 p-4 text-white">
-					<li><a href="#store">Store</a></li>
+					<li><a href="#Store">Store</a></li>
 				</ul>
 			</nav>
 			<Header/>

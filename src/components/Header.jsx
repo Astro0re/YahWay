d4272fa8c04297c836e00a1debd8c90a1a3a23ab
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-07 15:42:05 +0100

    Component edit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
     return( 
         <> 
-        <div className="y-4 text-bold">
+        <div style={{padding:'10px',textAlign:'center', textEmphasis:'bold'}} >
         <h1>Welcome to YahWay</h1>
-        <h2>LET'S HELP YOU GET CLOSER TO GOD TODAY</h2>
-        <h3>With our carefully scouted and trusted church partners</h3>
-        <h4>Let's connect you with a church to call home</h4>
+        <h2 style={{padding:'10px',textAlign:'center', textEmphasis:'bold'}}>LET'S HELP YOU GET CLOSER TO GOD TODAY</h2>
+        <h3 style={{padding:'10px',textAlign:'center', textEmphasis:'bold'}}>With our carefully scouted and trusted church partners</h3>
+        <h4 style={{padding:'10px',textAlign:'center', textEmphasis:'bold'}}>Let's connect you with a church to call home</h4>
         </div> 
         </>
     )

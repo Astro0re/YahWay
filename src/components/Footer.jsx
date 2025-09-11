a3511fc58801d4f617771192bfe818430b58bbd4
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-11 14:42:39 +0100

    Components Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,28 @@ import YahwayLogo from '../assets/Yahway Logo.svg'
 export default function footer() {
     return(
         <>
-            <h3>Contact Us</h3>
-            <p>Reach out to us on these platforms</p>
-            <div style={{display:'flex', flexDirection:'column', gap:'10px'}}>
-                
-                
-                <label> Email </label>
-                
-                
-                
-                <label> Number </label>
-                
             
+            <div style={{display:'flex', flexDirection:'row', gap:'10px'}}>
+                <div style={{display:'flex', flexDirection:'column', gap:'5px'}}>
+                    <h3>Contact Us</h3>
+                    <p>Reach out to us on these platforms</p>
+                </div>
+
+                <div style={{border: '1px', borderColor: 'gray', width:'1px', height:'10px'}}></div>
+
+                <div style={{display:'flex', flexDirection:'column', gap:'5px'}}>
+                    <p> Email </p>
+
+                    <p> Number </p>
                 
-                <label> Social </label>
+                    <p> Social </p>
+                </div>
+                
+                <div style={{border: '1px', borderColor: 'gray', width:'1px', height:'10px'}}></div>
+
+                <div style={{display:'flex', flexDirection:'column', gap:'5px'}}>
+                    <p>Terms and conditons</p>
+                </div>
                 
             </div>
             <p>&copy; 2025 YahWay. All Rights Reserved.</p>

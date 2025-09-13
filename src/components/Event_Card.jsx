5f73771d3845d3dfe6a05a2c89364069d65a03e4
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-13 18:35:35 +0100

    Update

--- src/components/Event_Card.jsx
+++ src/components/Event_Card.jsx
@@ -4,16 +4,17 @@ function adminadd(){
     Event_date= <input type="date" />
     Event_location = <input type="number" />
     Event_price = <input type="text" />
+    return Event_name,Event_date,Event_location,Event_price,
 }
 
 export default function Event_Card(){
     return(
         <>
-        <div style={{background:'black', textAlign:'center', color:'white', padding:'10px', borderRadius:'10px'}}>
+        <div style={{background:'black', textAlign:'center', color:'white', spadding:'10px', borderRadius:'10px'}}>
             <img  style={{height:'fit-content', width:'fit-content', borderRadius:'10px', padding:'5px'}}></img>
             <div style={{display:'flex', flexDirection:'column', gap:'20px', justifyContent:'center', padding:'4px', background:'white', color:'black', margin:'2px', borderRadius:'10px'}}>
                 <img style={{height:'fit-content', width:'fit-content', borderRadius:'10px', padding:'5px', borderBlockColor: 'black'}}></img>
-                <adminadd />
+                {adminadd()}
                 <p>(Event Name here)</p>
                 <p>(Date Here)</p>
                 <p>(Location Here)</p>

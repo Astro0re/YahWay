4bc31233d1c6c26c6c4ea214cab86b1a98d9c23d
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-14 19:57:25 +0100

    Sign up page

---
+++ src/pages/Sign_up.jsx
@@ -0,0 +1,15 @@
+function sign_up(){
+    return(
+        <div style={{margin:'200px', boarder:'white', }}>
+            <h1>Sign Up</h1>
+            <p>First Name</p>
+            <p>Last Name</p>
+            <p>Email</p>
+            <p>Password</p>
+            <p>Repeat Password</p>
+            <a href="">Terms and Conditions</a>
+        </div>
+    )
+}
+
+export default sign_up

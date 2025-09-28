07b6a21c10d52fe729712dd281fff8977776a7c9
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-28 16:47:38 +0100

    Javascript function update

--- src/components/Event_Card.jsx
+++ src/components/Event_Card.jsx
@@ -1,10 +1,22 @@
+<>
+<form>
+    <input type= "text" placeholder="Event Name" id="EN"></input>
+    <input type= "date" placeholder="Event Date" id="ED"></input>
+    <input type= "text" placeholder="Event Location" id="EL"></input>
+    <input type= "number" placeholder="Event Price" id="EP"></input>
+    <button type="submit" id="SUB" onClick={adminadd}>Add Event</button>
+</form>
+</>
+
 function adminadd(){
     //function to add events to the database
-    let Event_name= <input type="text" />
-    let Event_date= <input type="date" />
-    let Event_location = <input type="number" />
-    let Event_price = <input type="text" />
-    return Event_name,Event_date,Event_location,Event_price;
+    let count = {}
+    let Event_ID = count++
+    let Event_name= document.getElementById("EN")
+    let Event_date= document.getElementById("ED")
+    let Event_location = document.getElementById("EL")
+    let Event_price = document.getElementById("EP")
+    return Event_ID,Event_name,Event_date,Event_location,Event_price;
 }
 
 export default function Event_Card(){

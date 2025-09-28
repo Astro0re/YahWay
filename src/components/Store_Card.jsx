98d0644dad2ddfad9eb4706a548a8163b15c2d5d
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-28 17:05:33 +0100

    Update

--- src/components/Store_Card.jsx
+++ src/components/Store_Card.jsx
@@ -1,9 +1,20 @@
+<>
+<form>
+    <input type= "text" placeholder="Item Name" id="IN"></input>
+    <input type= "text" placeholder="Item Description " id="IDes"></input>
+    <input type= "number" placeholder="Item Price" id="IP"></input>
+    <button type="submit" id="SUB" onClick={adminadd}>Add Event</button>
+</form>
+</>
+
 function adminadd(){
     //function to add events to the database
+    let count={}
+    let Item_ID= count++
     let Item_name= <input type="text" />
     let Item_description= <input type="Desription" />
     let Item_price = <input type="text" />
-    return Item_name, Item_description, Item_price
+    return Item_ID,Item_name, Item_description, Item_price
 }
 
 export default function Store_Card(){

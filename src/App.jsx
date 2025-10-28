2ed71a35ac39c53e602f1adbad3b3c8773cdd84e
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-10-28 10:18:24 +0100

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Index from './pages/Index'
 import sign_up from './pages/sign_up'
+import store from './pages/Store'
+import Events from './pages/Events'
+import Maps from './pages/Maps'
+
 function App() {
 	return (
 		<Routes>
 			<Route path='/' element={<Index />} />
 			<Route path='/sign_up' element={<sign_up />} />
-			<Route path='/map' element />
-			<Route path='/shop' element />
-			<Route path='/event' element />
+			<Route path='/map' element={<Maps/>} />
+			<Route path='/shop' element={<store/>} />
+			<Route path='/event' element={<Events/>} />
 			<Route path='/partners' element />
 		</Routes>
 		)
